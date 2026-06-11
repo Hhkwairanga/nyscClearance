bb4b27269d34eb5958d15f2d1d78e1c5d1cecfbc
hhkwairanga <harunahk5575@gmail.com>
2026-06-11 17:04:23 +0100

    Add API root health response

--- nyscClearance_backend/nysc_clearance/urls.py
+++ nyscClearance_backend/nysc_clearance/urls.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 from accounts.views import verify_clearance
 from django.conf import settings
 from django.conf.urls.static import static
 
 
+def api_root(_request):
+    return JsonResponse({
+        'name': 'NYSC Clearance API',
+        'status': 'ok',
+        'version': getattr(settings, 'DEPLOYMENT_VERSION', ''),
+        'docs': '/api/auth/config/',
+    })
+
+
 urlpatterns = [
+    path('', api_root, name='api-root'),
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
     path('verify/', verify_clearance, name='verify-clearance'),
